3966e87a2b51feb08f0b9e561698fe73910b6f62
layonerM <79486043+layonerM@users.noreply.github.com>
2022-04-20 13:42:46 -0500

    creacion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,30 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
+ import Layout from "../components/layout"
 import Seo from "../components/seo"
+import LogoDataCurso from "../images/Logo-Data-Curso-e1595976981559.png"
+import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Dropdown  } from 'react-bootstrap';
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+
+     <div className="Header1">
+      <img src={LogoDataCurso} />
+    </div>
+
+  <div >
+    
+    
+
+  </div>
+
+
   </Layout>
+
 )
 
 export default IndexPage
--- src/pages/using-ssr.js
+++ src/pages/using-ssr.js
@@ -8,21 +8,7 @@ const UsingSSR = ({ serverData }) => {
   return (
     <Layout>
       <Seo title="Using SSR" />
-      <h1>SSR page</h1>
-      <img
-        style={{ width: "300px" }}
-        alt="A random dog"
-        src={serverData.message}
-      />
-      <p>Welcome to a server side rendered page with a random dog photo</p>
-      <p>
-        To learn more, head over to our{" "}
-        <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/">
-          documentation about Server Side Rendering
-        </a>
-        .
-      </p>
-      <Link to="/">Go back to the homepage</Link>
+      
     </Layout>
   )
 }
